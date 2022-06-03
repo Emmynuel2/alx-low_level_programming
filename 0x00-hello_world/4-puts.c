e4537096d513a627d65f9f01798ef3a5e0555f81
Your Name <you@example.com>
2022-06-02 18:07:54 -0700

    fdhf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 after printing the function (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
