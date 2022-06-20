87b67b0fc1fc35de1fd6682ea6c5a040e471ee03
Your Name <you@example.com>
2022-06-20 14:20:10 -0700

    fjf

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,15 +2,15 @@
 
 /**
  * _strchr - Locates a character in a string.
- * @s: The string to be searched.
+ * _strchr - Locates a character in a string
  * @c: The character to be located.
  *
  * Return: If c is found - a pointer to the first occurence.
- * 	   If c is not found - NULL.
+ *	If c is not found - NULL.
  */
 char *_strchr(char *s, char c)
 {
-	int index;c
+	int index;
 
 	for (index = 0; s[index] >= '\0'; index++)
 	{
@@ -18,5 +18,5 @@ char *_strchr(char *s, char c)
 			return (s + index);
 	}
 
-	return (s + index);
+	return ('\0');
 }
